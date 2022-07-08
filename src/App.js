cfc979fab85db9b9facf50e374fa6d94ed26273d
BenKer <benediktas.kertenis@codeacademylt.onmicrosoft.com>
2022-07-08 09:39:17 +0300

    App routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
-import "./App.css";
+import { Route, Routes, useNavigate } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import Register from "./components/Register";
+import Login from "./components/Login";
+import Add from "./components/Add";
 
-function App() {}
-
-export default App;
+export default function App() {
+  return (
+    <section>
+      <Navbar />
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/add" element={<Add />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </section>
+  );
+}
