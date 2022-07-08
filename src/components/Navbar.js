f9b8f4d1c4641efa5f00b941b8a808585421a163
BenKer <benediktas.kertenis@codeacademylt.onmicrosoft.com>
2022-07-08 12:22:31 +0300

    nav logged in/out. ismeciau mui, nemoku...

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,34 +1,39 @@
-import { Button } from "@mui/material";
 import { Link, useNavigate } from "react-router-dom";
 
-// Dar reikia log in/out funkciju
+export default function Navbar({ loggedIn, setLoggedIn }) {
+  const navigate = useNavigate();
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    setLoggedIn(false);
+    navigate("/login");
+  };
 
-export default function Navbar() {
   return (
     <nav>
       <Link to="/">
-        <img src="../../public/images/logo.svg" />
+        <img alt="logo" src="../../public/images/logo.svg" />
       </Link>
       <div>
-        <>
-          <Link to="/">
-            <Button variant="text">Home</Button>
-          </Link>
-          <Link to="/add">
-            <Button variant="text">Add</Button>
-          </Link>
-          <Button variant="text" color="error" /* onClick={} */>
-            Logout
-          </Button>
-        </>
-        <>
-          <Link to="/login">
-            <Button variant="text">Login</Button>
-          </Link>
-          <Link to="/register">
-            <Button>Register</Button>
-          </Link>
-        </>
+        {loggedIn ? (
+          <>
+            <Link to="/">
+              <button>Home</button>
+            </Link>
+            <Link to="/add">
+              <button>Add</button>
+            </Link>
+            <button onClick={handleLogout}>Logout</button>
+          </>
+        ) : (
+          <>
+            <Link to="/login">
+              <button>Log-in</button>
+            </Link>
+            <Link to="/register">
+              <button>Register</button>
+            </Link>
+          </>
+        )}
       </div>
     </nav>
   );
